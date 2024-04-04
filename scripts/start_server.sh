14098833aac2db8730a4c3ca818c1087c8a60f8e
Keval Indoriya <keval.i@simformsolutions.com>
2024-04-04 13:55:43 +0530

    script change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /project/backend
-echo `PORT=8000
+echo "PORT=8000
 MONGO_URI='mongodb://mongodb:27017/to-do'
 JWT_SECRET='secret key'
 GMAIL_USERNAME='admin@123'
-GMAIL_PASSWORD="abc@123"` >> .env
+GMAIL_PASSWORD='abc@123'" >> .env
 cd /project
 docker-compose build
 docker-compose -p webapp up -d
