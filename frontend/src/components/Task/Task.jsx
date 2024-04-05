e021ea8555513d0ab6b137344856eb2a9c078a21
Keval Indoriya <keval.i@simformsolutions.com>
2024-04-05 14:54:58 +0530

    modules removed

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -3,7 +3,6 @@ import moment from 'moment';
 import "./task.css";
 import { useContext } from 'react';
 import TaskContext from '../../context/TaskContext';
-import DeleteIcon from '@mui/icons-material/Delete';
 function Task({ task, id }) {
     const { dispatch } = useContext(TaskContext);
 
@@ -42,11 +41,11 @@ function Task({ task, id }) {
                 </div>
             </div>
             <div className="remove-task text-sm text-white">
-                <DeleteIcon
+                {/* <DeleteIcon
                     style={{ fontSize: 30, cursor: "pointer" }}
                     size="large"
                     onClick={handleRemove}
-                    className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1" />
+                    className="remove-task-btn bg-blue-700 rounded-full border-2 shadow-2xl border-white p-1" /> */}
             </div>
         </div>
     );
