140260cabaccf5a99b07a14c28c36d886cc7bd40
Keval Indoriya <125244023+keval-indoriya-simform@users.noreply.github.com>
2024-04-10 14:52:43 +0530

    Update axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-	baseURL:"http://54.242.128.54:8000/api"
+	baseURL:"http://http://to-do-backend-536036014.us-east-1.elb.amazonaws.com/:8000/api"
 })
 export default instance
